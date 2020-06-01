c69abaac7345cc2439a006eb63c012adcc568bb4
msmani <msmani9891@gmail.com>
2020-06-01 22:26:38 +0530

    Updates

---
+++ precache-manifest.f745750c56e95e931e46f07dea3f6c51.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe48e18d1536efd45692ccb1deb66d20",
+    "url": "/ReactMyProfile/index.html"
+  },
+  {
+    "revision": "720ccead8449350d9503",
+    "url": "/ReactMyProfile/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8e2dc1392fb4ff578e39",
+    "url": "/ReactMyProfile/static/css/main.17c47fdb.chunk.css"
+  },
+  {
+    "revision": "720ccead8449350d9503",
+    "url": "/ReactMyProfile/static/js/2.94f6f9c0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ReactMyProfile/static/js/2.94f6f9c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e2dc1392fb4ff578e39",
+    "url": "/ReactMyProfile/static/js/main.ae4c931b.chunk.js"
+  },
+  {
+    "revision": "49dc360a8ddf70630aeb",
+    "url": "/ReactMyProfile/static/js/runtime-main.218a4309.js"
+  },
+  {
+    "revision": "e41d06f66baa5db6a211e17d004be4d4",
+    "url": "/ReactMyProfile/static/media/corestack.e41d06f6.png"
+  },
+  {
+    "revision": "3bf68646e4ce9c2b160d4ef8b435094b",
+    "url": "/ReactMyProfile/static/media/kendo.3bf68646.png"
+  },
+  {
+    "revision": "552d8406856140dd3fe50318e5e6a37c",
+    "url": "/ReactMyProfile/static/media/lynk.552d8406.PNG"
+  },
+  {
+    "revision": "e113b5a6718e89722535ab0cca0f0987",
+    "url": "/ReactMyProfile/static/media/merlin.e113b5a6.png"
+  },
+  {
+    "revision": "1dafc3f3c0c21f6dfd78f80a317997cd",
+    "url": "/ReactMyProfile/static/media/mine.1dafc3f3.jpg"
+  },
+  {
+    "revision": "cca24d2443b3ad259b10d657d0ab6258",
+    "url": "/ReactMyProfile/static/media/pc.cca24d24.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.9632006f74a78d30fa736aef83067177.js"
+  "/ReactMyProfile/precache-manifest.f745750c56e95e931e46f07dea3f6c51.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactMyProfile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
