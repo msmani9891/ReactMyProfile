73cc3295e76a1c280b16c085fbb75742ecaaee4b
msmani <msmani9891@gmail.com>
2020-06-01 21:41:38 +0530

    Updates

---
+++ precache-manifest.9632006f74a78d30fa736aef83067177.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a67fd88deb9da4728e4920ac5687d2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "720ccead8449350d9503",
+    "url": "/my-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8067feca4f1596096573",
+    "url": "/my-app/static/css/main.17c47fdb.chunk.css"
+  },
+  {
+    "revision": "720ccead8449350d9503",
+    "url": "/my-app/static/js/2.94f6f9c0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-app/static/js/2.94f6f9c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8067feca4f1596096573",
+    "url": "/my-app/static/js/main.014c8e9a.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  },
+  {
+    "revision": "e41d06f66baa5db6a211e17d004be4d4",
+    "url": "/my-app/static/media/corestack.e41d06f6.png"
+  },
+  {
+    "revision": "3bf68646e4ce9c2b160d4ef8b435094b",
+    "url": "/my-app/static/media/kendo.3bf68646.png"
+  },
+  {
+    "revision": "552d8406856140dd3fe50318e5e6a37c",
+    "url": "/my-app/static/media/lynk.552d8406.PNG"
+  },
+  {
+    "revision": "e113b5a6718e89722535ab0cca0f0987",
+    "url": "/my-app/static/media/merlin.e113b5a6.png"
+  },
+  {
+    "revision": "1dafc3f3c0c21f6dfd78f80a317997cd",
+    "url": "/my-app/static/media/mine.1dafc3f3.jpg"
+  },
+  {
+    "revision": "cca24d2443b3ad259b10d657d0ab6258",
+    "url": "/my-app/static/media/pc.cca24d24.PNG"
+  }
+]);
